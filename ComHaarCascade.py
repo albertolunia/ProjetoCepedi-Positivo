b178afcb7805341d2839b7f8080dee96964f7bdb
Alberto Henrique <albertolunia@gmail.com>
2023-03-26 18:13:18 -0300

    Atualizando

--- ComHaarCascade.py
+++ ComHaarCascade.py
@@ -4,25 +4,28 @@ from imutils import paths
 
 
 def detectar():
-    imagemPath = list(paths.list_images('imagens/'))
+    imagemPath = list(paths.list_images('img-placa-144307'))
     numero = 0
     detector_face = cv2.CascadeClassifier('cascade.xml')
     for z in imagemPath:
         img = cv2.imread(z)
         img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         deteccoes = detector_face.detectMultiScale(img_cinza)
+        print(f"Foto {numero}")
         for (x, y, l, a) in deteccoes:
             imgbb = img_cinza[y:y + a, x:x + l]
-            circles = cv2.HoughCircles(imgbb, cv2.HOUGH_GRADIENT, 1, 120, param1=100, param2=20, minRadius=10, maxRadius=20)
+            circles = cv2.HoughCircles(imgbb, cv2.HOUGH_GRADIENT, 1, 120, param1=100, param2=20, minRadius=10,
+                                       maxRadius=20)
             if isinstance(circles, np.ndarray):
                 circles = np.uint16(np.around(circles))
 
                 for i in circles[0, :]:
                     cv2.circle(img, (x + i[0], y + i[1]), i[2], (0, 255, 0), 2)
                     cv2.circle(img, (x + i[0], y + i[1]), 2, (0, 0, 255), 2)
+                    print(f"(x,y): ({x + i[0]},{y + i[1]})")
         cv2.imshow("Com Haar Cascade", img)
-        cv2.waitKey(800)
-        cv2.imwrite('IdentificadasHaar/' + str(numero) + '.png', img)
+        cv2.waitKey(1000)
+        cv2.imwrite('haar-placa-144307/' + str(numero) + '.png', img)
         numero += 1
 
 
--- SemHaarCascade.py
+++ SemHaarCascade.py
@@ -4,24 +4,25 @@ from imutils import paths
 
 
 def detectar():
-    imagemPath = list(paths.list_images('imagens/'))
+    imagemPath = list(paths.list_images('img-placa-144307/'))
     numero = 0
     for z in imagemPath:
         img = cv2.imread(z)
         img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img2 = cv2.medianBlur(img_cinza, 5)
-
+        print(f"Foto {numero}")
         circles = cv2.HoughCircles(img2, cv2.HOUGH_GRADIENT, 1, 120, param1=100, param2=20, minRadius=10, maxRadius=20)
         if isinstance(circles, np.ndarray):
             circles = np.uint16(np.around(circles))
 
             for i in circles[0, :]:
                 cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
-                cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 1)
+                cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 2)
+                print(f"(x,y): ({i[0]},{i[1]})")
 
         cv2.imshow("Sem Haar Cascade", img)
-        cv2.waitKey(0)
-        cv2.imwrite('IdentificadasSemHaar/' + str(numero) + '.png', img)
+        cv2.waitKey(1000)
+        cv2.imwrite('Shaar-placa-144307/' + str(numero) + '.png', img)
         numero += 1
 
 
